bf0e53a0e3866862ae9220ca6d85da86b0af5304
xuanbaojr <100544563+xuanbaojr@users.noreply.github.com>
2023-04-20 23:02:44 +0700

    chua them bootstrap

--- resources/js/components/test.vue
+++ resources/js/components/test.vue
@@ -1,15 +1,19 @@
 <template>
     <button class="btn" @click="test">Count: {{ count }}</button>
+
+    <p>Using text interpolation: {{ rawHtml }}</p>
+    <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+    <div v-bind:id="dynamicId"></div>
+
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-    const count = ref(0);
-    function test(){
-        count.value++;
-    }
-
-    onMounted(()=> {
-        console.log('xuanbao');
-    })
+import {ref, onMounted} from 'vue'
+const count = ref(0)
+function test(){
+    count.value++
+}
+onMounted(()=>{
+    console.log('xuanbao01')
+})
 </script>
