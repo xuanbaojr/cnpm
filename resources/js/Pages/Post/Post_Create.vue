b8640792c942b39716b7edd7f35144271f30c6b2
xuanbaojr <100544563+xuanbaojr@users.noreply.github.com>
2023-05-10 11:29:26 +0700

    Update Post_Create.vue

--- resources/js/Pages/Post/Post_Create.vue
+++ resources/js/Pages/Post/Post_Create.vue
@@ -21,7 +21,7 @@
                     {{ city.Name }}
             </option>
           </select>
-          <label class="des-tilte">Quận/Huyện 01</label>
+          <label class="des-tilte">Quận/Huyện 01 02</label>
           <select class="form-select" id="test" v-model="form.district">.
             <option>Quận / Huyện</option>
             <option v-for="district  in districts"
